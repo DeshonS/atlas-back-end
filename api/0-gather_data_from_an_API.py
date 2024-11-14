fe868fb23e62095673b1e4081ba8d69a6bfd0848
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2024-11-14 14:27:59 -0600

    fixing pep8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,19 +3,22 @@
 import requests
 import sys
 
+
 def emp_todo(employee_id):
-    uurl = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    turl = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    uurl = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    turl = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        employee_id)
     user_response = requests.get(uurl)
     user_data = user_response.json()
     employee_name = user_data.get("name")
     td_response = requests.get(turl)
     tdata = td_response.json
-    
+
     total_tasks = len(tdata)
-    completed_tasks = [task for task in tdata if task["completed"]]
+    completed_tasks = [task for task in tdata if
+                       task["completed"]]
     number_of_finished_tasks = len(completed_tasks)
-    
     print("Employee {} is done with tasks({}/{}):".format(number_of_finished_tasks, total_tasks))
     for task in completed_tasks:
         print("\t {}".format({task['title']}))
