7d0e9559e198c06b25a2776da037c8fd3c3a0021
DeshonS <deshonjewell@gmail.com>
2024-11-14 14:44:27 -0600

    corrected code errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,16 +13,17 @@ def emp_todo(employee_id):
     user_data = user_response.json()
     employee_name = user_data.get("name")
     td_response = requests.get(turl)
-    tdata = td_response.json
+    tdata = td_response.json()
 
     total_tasks = len(tdata)
     completed_tasks = [task for task in tdata if
                        task["completed"]]
     number_of_finished_tasks = len(completed_tasks)
-    print("Employee {} is done with tasks({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
         number_of_finished_tasks, total_tasks))
     for task in completed_tasks:
-        print("\t {}".format({task['title']}))
+        print("\t {}".format(task['title']))
+
 
     if __name__ == "__main__":
         emp_todo(employee_id)
